0a3269c0a324cfbb28d0b5e0a39c25565fb73247
Leandro Prado <57591950+leandro998@users.noreply.github.com>
2020-05-25 11:00:38 -0700

    first commit

---
+++ Mini Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini Project
+//
+//  Created by Leandro Prado on 2020-05-25.
+//  Copyright © 2020 Leandro Prado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
