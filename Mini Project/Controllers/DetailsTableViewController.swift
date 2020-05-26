92894d13e96fb4e5d4272af44d07e5f8d8e6b23f
Leandro Prado <57591950+leandro998@users.noreply.github.com>
2020-05-26 13:46:55 -0700

    DetailsTVC title update

--- Mini Project/Controllers/DetailsTableViewController.swift
+++ Mini Project/Controllers/DetailsTableViewController.swift
@@ -20,6 +20,9 @@ class DetailsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let unknown = "N/A"
+        
+        navigationItem.title = "\(data.title) - Details"
+        
         totalCasesLabel.text = data.data[.totalCases]?.description ?? unknown
         totalDeathsLabel.text = data.data[.totalDeaths]?.description ?? unknown
         totalRecoveryLabel.text = data.data[.totalRecoveries]?.description ?? unknown
@@ -63,8 +66,8 @@ class DetailsTableViewController: UITableViewController {
         
 //        return cell
 //    }
-
-
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
